c7d06bc877247880a415a4b63212c22a1e7b55fc
AbonyiXavier <francisabonyi@gmail.com>
2021-08-11 05:45:05 +0100

    Github action

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -5,9 +5,16 @@ module.exports = {
     url: process.env.DEV_DATABASE_URL,
     dialect: 'postgres',
   },
+  // test: {
+  //   url: process.env.TEST_DATABASE_URL,
+  //   dialect: 'postgres',
+  // },
   test: {
-    url: process.env.TEST_DATABASE_URL,
-    dialect: 'postgres',
+    username: "postgres",
+    password: "postgres",
+    database: "postgres",
+    host: "postgres",
+    dialect: "postgres"
   },
   production: {
     url: process.env.DATABASE_URL,
